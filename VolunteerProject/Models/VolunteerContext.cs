4044e03d7d40a2d59d30984cb8d3911c89909b3b
Foster Stule <camodude5@yahoo.com>
2018-11-13 19:08:01 -0500

    removed forums and comments to help simplify the project

--- VolunteerProject/Models/VolunteerContext.cs
+++ VolunteerProject/Models/VolunteerContext.cs
@@ -12,8 +12,7 @@ namespace VolunteerProject.Models
 
 		public DbSet<User> Users { get; set; }
 		public DbSet<Event> Events { get; set; }
-		public DbSet<Forum> Forums { get; set; }
-		public DbSet<Comment> Comments { get; set; }
+
 		public DbSet<Chat> Chats { get; set; }
 		public DbSet<Message> Messages { get; set; }
 	}
